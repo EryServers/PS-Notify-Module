c9f92c98051d7974d2cc592e454187d13244cbc0
Eryniox <Eryniox@users.noreply.github.com>
2026-04-27 11:33:56 +0200

    Add -UseBasicParsing to Invoke-WebRequest in SendPayload method

--- Discord-WebHook.psm1
+++ Discord-WebHook.psm1
@@ -279,7 +279,7 @@ class DiscordWebHook {
     hidden [void] SendPayload([hashtable]$Payload) {
         # Write-Host "DEBUG: Invoke-WebRequest -Uri $($this.ThreadURL) -Method Post"
         # Write-Host ("DEBUG: -Body `r`n" + ($Payload | ConvertTo-Json -Depth 8))
-        Invoke-WebRequest -Uri $this.ThreadURL -Method Post -Body ($Payload | ConvertTo-Json -Depth 8) -ContentType "application/json"
+        Invoke-WebRequest -Uri $this.ThreadURL -Method Post -Body ($Payload | ConvertTo-Json -Depth 8) -ContentType "application/json" -UseBasicParsing
     }
 
     [void] AddEmbed([DiscordEmbed]$embed) {
